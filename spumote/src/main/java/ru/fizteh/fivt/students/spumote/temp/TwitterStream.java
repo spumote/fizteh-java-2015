3908a1320961b7630c73a5db08405607ff17d499
Nail Mingaliev <mingaliev122@mail.ru>
2015-12-18 03:40:29 +0300

    task5

---
+++ spumote/src/main/java/ru/fizteh/fivt/students/spumote/temp/TwitterStream.java
@@ -0,0 +1,7 @@
+package ru.fizteh.fivt.students.spumote.temp;
+
+/**
+ * Created by spumote on 18.12.15.
+ */
+public class TwitterStream {
+}
---
+++ spumote/src/main/java/ru/fizteh/fivt/students/spumote/twitterstream/CommandLine.java
@@ -0,0 +1,7 @@
+package ru.fizteh.fivt.students.spumote.twitterstream;
+
+/**
+ * Created by spumote on 17.12.15.
+ */
+public class CommandLine {
+}
